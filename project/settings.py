825f42313c16b1e447a2ee6ef0a79b730a2f598a
luk3mn <lucasrenan2015@gmail.com>
2022-06-29 11:31:35 -0300

    correção: corrigindo o nome da variavel SECRET_KEY

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECREAT_KEY') # passar o valor da variável de ambiente
+SECRET_KEY = os.getenv('SECRET_KEY') # passar o valor da variável de ambiente
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False) # associa a variável de ambiente e define ele com o valor padrão 'False'
