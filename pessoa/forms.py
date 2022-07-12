a8a30c9b5335db4f1ac48ee2de6172a6931b8ef5
luk3mn <lucasrenan2015@gmail.com>
2022-07-12 10:20:07 -0300

    Realizando o update, delete e filtro de busca de pessoas

--- pessoa/forms.py
+++ pessoa/forms.py
@@ -2,6 +2,13 @@ from django import forms # para auxiliar na criação do usuário que interage c
 from .models import Pessoa
 
 class PessoaForm(forms.ModelForm):
+    # exibe o campo 'Data Nascimento' no formato de 'Date'
+    data_nascimento = forms.DateField(
+        widget=forms.TextInput(
+            attrs={"type":"date"}
+        )
+    )
+
     class Meta:
         model = Pessoa # para saber com qual model ele precisa fazer referencia
         fields = ['nome_completo', 'data_nascimento', 'ativa']
--- pessoa/views.py
+++ pessoa/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # 'ListView' Própria para o desenvolvimento de telas de listagem de informação
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import Pessoa # model que será realizado a listagem
 from .forms import PessoaForm # importa o formulário criado
 
@@ -9,8 +9,28 @@ class ListaPessoaView(ListView):
     model = Pessoa
     queryset = Pessoa.objects.all().order_by('nome_completo') # Consulta SQL -> retorna todos os registros ordenados pelo nome completo
 
+    # mecanismo de busca
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        filtro_nome = self.request.GET.get('nome') or None
+
+        if filtro_nome: # verifica se contem o registro passado no filtro
+            queryset = queryset.filter(nome_completo__contains=filtro_nome)
+        
+        return queryset
+
 
 class PessoaCreateView(CreateView):
     model = Pessoa # Indica qual é o model de referencia
     form_class = PessoaForm
     success_url = '/pessoas/' # redireciona para outra página (pessoas) quando o cadastro tiver sido realizado
+
+class PessoaUpdateView(UpdateView): # só muda a classe de herança, o princípio é o mesmo
+    model = Pessoa
+    form_class = PessoaForm
+    success_url = '/pessoas/'
+
+# Para deletar não precisa recuperar os dados do formulario
+class PessoaDeleteView(DeleteView):
+    model = Pessoa
+    success_url = '/pessoas/'
